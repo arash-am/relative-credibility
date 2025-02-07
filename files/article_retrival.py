753edfa546e7777ddf43584e726d70b755ef498f
Arash Amini <arashamini1372@gmail.com>
2025-02-06 18:01:46 -0600

    fixed owerrwiting errors

--- files/article_retrival.py
+++ files/article_retrival.py
@@ -16,7 +16,7 @@ def fetch_article_content(url,attempts = 3,delay = 2):
                 "text": article.text
             }
         except Exception as e:
-            print(f"\nAttempt {attempt + 1} failed for {url}: {e}")
+            # print(f"\nAttempt {attempt + 1} failed for {url}: {e}")
             if attempt < attempts - 1:
                 time.sleep(min(delay, 60))  # Ensure the max delay is 1 minute
                 delay *= 2  # Exponential backoff
@@ -26,20 +26,25 @@ def fetch_article_content(url,attempts = 3,delay = 2):
 # Assuming single_topic DataFrame has a column 'url'
 
 bad_count=0
-df=read_plot(file_path='../reddit-data/ukraine_russia.csv')
+df=read_plot(file_path='reddit-data/ukraine_russia.csv')
+df_loaded=pd.read_csv('reddit-data/ukraine_russia_article.csv')
 df["article_title"]=''
 df["article_text"]=''
-for i,url in enumerate(tqdm(df["url"].values)):
-    if df.iloc[i]["article_title"]=='':
-        article = fetch_article_content(url)
+for i, row in tqdm(df.iterrows(), total=len(df)):
+    # if row["title"]
+    if not df_loaded[row['title']==df_loaded["article_title"]].empty:
+        df.at[i, "article_title"] = df_loaded[row['title']==df_loaded["article_title"]]["article_title"]
+        df.at[i, "article_text"] = df_loaded[row['title']==df_loaded["article_title"]]["article_text"]
+    elif pd.isna(row["article_title"]) or row["article_title"] == '':
+        article = fetch_article_content(row["url"])
         if article["title"]:
-            df.at[i,"article_title"] = article["title"]
+            df.at[i, "article_title"] = article["title"]
             df.at[i, "article_text"] = article["text"]
         else:
             bad_count+=1
-    if i%20==0 and i>0:
+    if i%100==0 and i>0:
         print(2*"\n","*"*100)
         print(f"Processed {i} articles with retrival ratio: {1- bad_count/(i+1)}")
         print("*" * 100,2 * "\n")
-        df.to_csv('../reddit-data/ukraine_russia_article.csv', index=False)
-df.to_csv('../reddit-data/ukraine_russia_article.csv', index=False)
+        df.to_csv('reddit-data/ukraine_russia_article.csv', index=False)
+df.to_csv('reddit-data/ukraine_russia_article.csv', index=False)
--- files/read_data.py
+++ files/read_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def read_plot(file_path='../reddit-data/ukraine_russia.csv'):
+def read_plot(file_path='reddit-data/ukraine_russia.csv'):
         df=pd.read_csv(file_path)
         print(f'Keyword filter: {len(df)}')
         df['week'] = pd.to_datetime(df['created_utc'], unit='s', utc=True).dt.to_period('W')
@@ -12,19 +12,19 @@ def read_plot(file_path='../reddit-data/ukraine_russia.csv'):
         domain_counts=domain_counts[domain_counts['count']>100]
         df=df[df['domain'].isin(domain_counts['domain'].unique())]
         print(f'Domain frequency filter: {len(df)}')
-
-        weekly_counts = df.groupby('week').count()['id'].reset_index()
         # Plotting
-        fig, ax = plt.subplots(figsize=(12, 6))
-        ax.bar(weekly_counts['week'].astype(str), weekly_counts['id'], color='skyblue',width=1)
-        ax.set_xlabel('Week', fontsize=12)
-        ax.set_ylabel('Count of ID', fontsize=12)
-        ax.set_title('Weekly ID Counts', fontsize=14, fontweight='bold')
-        num_ticks = 10  # Adjust based on your data size
-        ax.set_xticks(ax.get_xticks()[::max(len(weekly_counts) // num_ticks, 1)])
-        ax.tick_params(axis='x', rotation=45)
-        # Grid lines for better readability
-        ax.grid(axis='y', linestyle='--', alpha=0.7)
-        # Show the plot
-        plt.show()
+        if 0:
+                fig, ax = plt.subplots(figsize=(12, 6))
+                weekly_counts = df.groupby('week').count()['id'].reset_index()
+                ax.bar(weekly_counts['week'].astype(str), weekly_counts['id'], color='skyblue',width=1)
+                ax.set_xlabel('Week', fontsize=12)
+                ax.set_ylabel('Count of ID', fontsize=12)
+                ax.set_title('Weekly ID Counts', fontsize=14, fontweight='bold')
+                num_ticks = 10  # Adjust based on your data size
+                ax.set_xticks(ax.get_xticks()[::max(len(weekly_counts) // num_ticks, 1)])
+                ax.tick_params(axis='x', rotation=45)
+                # Grid lines for better readability
+                ax.grid(axis='y', linestyle='--', alpha=0.7)
+                # Show the plot
+                plt.show()
         return df
